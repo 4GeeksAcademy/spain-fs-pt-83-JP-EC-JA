06be58990cb21f88c30c812d5e9ee452efada093
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2025-01-15 17:43:02 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,28 +1,31 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		
+	<footer className="footer mt-5 p-0 text-center">
+
 		<div className="container-fluid">
 			<div className="row justify-content-center row-cols-1 row-cols-sm-2 row-cols-md-3">
-				<div className="col">
+				<div className="col px-3 py-2">
 					<h5 className="text-uppercase text-primary">nosotros</h5>
 					<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
 					</p>
 				</div>
 
-				<div className="col">
+				<div className="col px-3 py-2">
 					<h5 className="text-uppercase text-primary">politica</h5>
 					<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
 					</p>
 				</div>
 
-				<div className="col">
+				<div className="col px-3 py-2">
 					<h5 className="text-uppercase text-primary">contactanos</h5>
 					<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+						<i class="bi bi-envelope"></i> test@test.com
+					</p>
+					<p>
+						<i class="bi bi-telephone"></i> +34 000 00 00
 					</p>
 				</div>
 			</div>
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,16 +10,14 @@ export const Navbar = () => {
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<div className="col"></div>
-					<div className="col-3">
+					
+					<div className="col-md-6 offset-md-2 p-1">
 						<form className="d-flex" role="search">
 							<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
 							<button className="btn btn-outline-success" type="submit">Search</button>
 						</form>
 					</div>
-					<div className="col">
-
-					</div>
+					
 					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 						<li className="nav-item dropdown">
 							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
