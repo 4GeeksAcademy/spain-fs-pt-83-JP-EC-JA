dfc4f86936ca7e4bfe488d620b4c625c4d674cc2
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2025-01-15 17:09:40 +0000

    haciendo el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,40 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
+		
+		<div className="container-fluid">
+			<div className="row justify-content-center row-cols-1 row-cols-sm-2 row-cols-md-3">
+				<div className="col">
+					<h5 className="text-uppercase text-primary">nosotros</h5>
+					<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+					</p>
+				</div>
+
+				<div className="col">
+					<h5 className="text-uppercase text-primary">politica</h5>
+					<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+					</p>
+				</div>
+
+				<div className="col">
+					<h5 className="text-uppercase text-primary">contactanos</h5>
+					<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum exercitationem quas, distinctio reiciendis quisquam, vel eligendi quae aut esse, quasi ipsa sit neque tempore dignissimos hic ut ad! Recusandae, iusto.
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+		<hr></hr>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Copyright Todos los Derechos Reservados
 		</p>
+		<i class="bi bi-facebook"></i>
+		<i class="bi bi-instagram"></i>
+		<i class="bi bi-twitter-x"></i>
+		<i class="bi bi-youtube"></i>
 	</footer>
 );
