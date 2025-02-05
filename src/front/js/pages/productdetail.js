7c3676d3a76d64653ac5148745369237150d4f4f
Judit <judit.arman95@gmail.com>
2025-02-05 20:37:02 +0000

    Vistas detalle algo css

--- src/front/js/pages/productdetail.js
+++ src/front/js/pages/productdetail.js
@@ -20,25 +20,25 @@ export const ProductDetail = () => {
     return (
 
         <div className="detail-card m-5 p-5">
-            <div className="detail">
-                <div className="img-card">
-                    <img src={store.productdetail.image} className="card-img" alt="..."></img>
-                </div>
-                <div className="body-text m-5 p-5">
-                    <h5 className="title">{store.productdetail.title}</h5>
-                    <p>{store.productdetail.description}</p>
-                    <p>{store.productdetail.price}€</p>
-                    <h6>{store.productdetail.rating.rate}</h6>
-                    <input type="number" className="number" min="0" max="100" placeholder="0" />
-                    <a href="#" className="btn btn-light btn-all">
-                        {
-                            store.favorites.includes(store.productdetail.id) ?
-                                <i onClick={() => actions.removeFavorite(store.productdetail.id)} className="fa-solid fa-heart"></i> :
-                                <i onClick={() => actions.addFavorite(store.productdetail.id)} className="bi bi-heart"></i>
-                        }
-                    </a>
-                    <a href="#" className="btn btn-light btn-all"><i className="bi bi-cart3"></i></a>
-                </div>
+
+            <div className="img-card">
+                <img src={store.productdetail.image} className="card-img" alt="..."></img>
+            </div>
+            <div className="body-text m-5 p-5">
+                <h5 className="title">{store.productdetail.title}</h5>
+                <p>{store.productdetail.description}</p>
+                <p>{store.productdetail.price}€</p>
+                <h6>{store.productdetail.rating.rate}</h6>
+                <input type="number" className="number" min="0" max="100" placeholder="0" />
+                <a href="#" className="btn btn-light btn-all">
+                    {
+                        store.favorites.includes(store.productdetail.id) ?
+                            <i onClick={() => actions.removeFavorite(store.productdetail.id)} className="fa-solid fa-heart"></i> :
+                            <i onClick={() => actions.addFavorite(store.productdetail.id)} className="bi bi-heart"></i>
+                    }
+                </a>
+                <a href="#" className="btn btn-light btn-all"><i className="bi bi-cart3"></i></a>
+
             </div>
 
 
