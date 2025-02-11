8d7edeac9814a0c16456178e59d99c59e53d2346
Judit <judit.arman95@gmail.com>
2025-02-11 19:04:34 +0000

    algo no se veia

--- src/api/routes.py
+++ src/api/routes.py
@@ -163,6 +163,8 @@ def handle_get_cart(id):
         return jsonify({'msg': 'Producto en el carrito no encontrado'}), 404
     return jsonify(cart.serialize()), 200
     
+
+
 #----------------------------Crear Cart ---------------------------
 
 @api.route('/cart', methods=['POST'])
