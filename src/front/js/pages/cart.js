d785bab927e1e0a60ddf2720e715bc4322308f61
Judit <judit.arman95@gmail.com>
2025-02-12 21:30:32 +0000

    trash agregada carrito

--- src/front/js/pages/cart.js
+++ src/front/js/pages/cart.js
@@ -9,21 +9,15 @@ export const Cart = () => {
 
     return (
         <div className="card-container">
-            <div className="row justify-content-center">
+            <div className="row d-flex justify-content-center">
                 {cartProducts.length > 0 ? (
                     cartProducts.map(product => (
                         <div key={product.id} className="card border-1 shadow px-5 py-2 text-center">
                             <img src={product.image} className="card-img-top" alt="imagenes de los productos" />
                             <div className="card-body p-1 m-1">
                                 <h5 className="card-title">{product.title}</h5>
-                                <p>{product.category}</p>
-                                <p>{product.rating.rate}</p>
                             </div>
-                            <div className="card-footer d-flex bg-white border-0">
-                                <Link className="btn btn-light" to={`/detail/${product.id}`}>More info</Link>
-                                <Link className="btn btn-light" to="/">
-                                    <i onClick={() => actions.addFavorite(id)} className="bi bi-heart"></i>
-                                </Link>
+                            <div className="card-footer bg-white border-0">
                                 <button className="btn btn-light btn-all" onClick={() => actions.removeCart(product.id)}>
                                     <i class="bi bi-trash3"></i>
                                 </button>
