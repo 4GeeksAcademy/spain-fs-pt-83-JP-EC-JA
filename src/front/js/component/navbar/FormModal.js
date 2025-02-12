39ad4db0c72210b7c785bfd51d7f0ee2d60a927e
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2025-02-12 00:13:20 +0000

    mejoras responsive

--- src/front/js/component/navbar/FormModal.js
+++ src/front/js/component/navbar/FormModal.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from "react";
 import "../../../styles/modal.css";
 import { Context } from "../../store/appContext";
-import { string } from "prop-types";
 
 
 
