3ae3109ff484543b58aa2dabdbe2a62378dd0213
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2025-02-12 21:16:48 +0000

    a

--- src/front/js/pages/perfil.js
+++ src/front/js/pages/perfil.js
@@ -1,26 +1,31 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-
+import avatar from "../../img/avatar.png"
 
 export const Perfil = () => {
 
     const { store, actions } = useContext(Context);
-    const {user} = store;
-    
-    if (!user){
+    const { user } = store;
+
+    if (!user) {
         return null
     }
 
     return (
-        <div className="pt-5 mt-5">
+        <div className="perfil text-center border border-1 rounded-4 shadow">
             <h2 className="text-center">Perfil del usuario</h2>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <img src={avatar} />
+                    </div>
+                    <div className="col">
+                        <h5 className="card-title">Nombre de Usuario: {user.username}</h5>
+                        <p className="card-text">Nombre: {user.name}</p>
+                        <p className="card-text">Apellido: {user.lastname}</p>
+                        <p className="card-text">Correo: {user.email}</p>
+                    </div>
 
-            <div class="card shadow">
-                <div class="card-body">
-                    <h5 class="card-title">Nombre de Usuario: {user.username}</h5>
-                    <p class="card-text">Nombre: {user.name}</p>
-                    <p class="card-text">Apellido: {user.lastname}</p>
-                    <p class="card-text">Correo: {user.email}</p>
                 </div>
             </div>
 
