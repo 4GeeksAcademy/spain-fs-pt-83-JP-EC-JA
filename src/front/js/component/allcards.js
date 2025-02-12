68e8b1635359ef1fe1bc3ee0817e63763da50065
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2025-02-12 14:04:58 +0000

    Revert "se iguala el tamaño de los tirulos de las cards parafijar la ubicacion de los iconos"
    This reverts commit 762276aa027e01c601b0ca984f82de7a64f8406c.

--- src/front/js/component/allcards.js
+++ src/front/js/component/allcards.js
@@ -12,9 +12,7 @@ export const AllCards = ({ image, title, category, price, rate, id, }) => {
         <div className="card-products col-3">
             <img src={image} className="card-img" alt="..." />
             <div className="card-Shop">
-                <div className="prduct-title">
-                    <h5 className="card-title">{title}</h5>
-                </div>                
+                <h5 className="card-title">{title}</h5>
                 <p>{category}</p>
                 <p>{price}€</p>
                 <h6>{rate}</h6>
