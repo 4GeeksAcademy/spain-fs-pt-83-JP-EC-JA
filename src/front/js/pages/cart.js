c846e713fb0ad764389d28f59b8464cfb871a293
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2025-02-13 15:10:12 +0000

    responsive carts casi ok

--- src/front/js/pages/cart.js
+++ src/front/js/pages/cart.js
@@ -4,57 +4,70 @@ import { Context } from "../store/appContext";
 
 export const Cart = () => {
     const { store, actions } = useContext(Context);
-    const cartProducts = store.products.filter(product => store.cart.includes(product.id));    
+    const cartProducts = store.products.filter(product => store.cart.includes(product.id));
 
     return (
-        <div className="cart-container text-center">
+        <div className="cart-container-head text-center">
             <div className="row row-cart-head">
-                <div className="col-4">
+                <div className="col-12 col-md-6">
                     <h2>Producto</h2>
                 </div>
-                <div className="col-2">
+                <div className="col">
                     <h2>precio</h2>
                 </div>
-                <div className="col-2">
+                <div className="col">
                     <h2>Cantidad</h2>
                 </div>
-                <div className="col-2">
+                <div className="col">
                     <h2>Total</h2>
                 </div>
-                <div className="col-2">
+                <div className="col">
                     <h2>Accion</h2>
                 </div>
             </div>
-            {cartProducts.length > 0 ? (
-                cartProducts.map(product => (             
-                    <div key={product.id} className="row row-cart cols-1 cols-md-2 cols-lg-5">                       
-                        <div className="col-4 d-flex align-items-center">
-                            <img src={product.image} className="cart-img" alt="imagenes de los productos" />                     
-                            <div className="title-cart-img">
-                                <h5>{product.title}</h5>                                
-                            </div> 
+            {
+                cartProducts.length > 0 ? (
+                    cartProducts.map(product => (
+                        <div className="cart-container-products text-center">
+                            <div key={product.id} className="row cols-2 cols-md-1 row-cart-products">
+                                <div className="col-12 col-md-6">
+                                    <div className="row cols-1 cols-md-2 row-cart-img-title">
+                                        <div className="col-12 col-md-6">
+                                            <img src={product.image} className="cart-img" alt="imagenes de los productos" />
+                                        </div>
+                                        <div className="col-12 col-md-6 title-cart-img">
+                                            <h5>{product.title}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col cart-characteristics">
+                                    <div className="row cols-4  row-cart-characteristics">
+                                        <div className="col">
+                                            <h3>{product.price}€</h3>
+                                        </div>
+                                        <div className="col">
+                                            <h3>Cantidad</h3>
+                                        </div>
+                                        <div className="col">
+                                            <h3>Total</h3>
+                                        </div>
+                                        <div className="col">
+                                            <button className="btn btn-light btn-all" onClick={() => actions.removeCart(product.id)}>
+                                                <i class="bi bi-trash3"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col">
-                            <h3>{product.price}€</h3>
+                    ))
+                ) :
+                    (
+                        <div className="no-cart">
+                            <p>Tu carrito está vacío</p>
                         </div>
-                        <div className="col">
-                            <h1>Cantidad</h1>
-                        </div>
-                        <div className="col">
-                            <h1>Total</h1>
-                        </div>
-                        <div className="col">
-                            <button className="btn btn-light btn-all" onClick={() => actions.removeCart(product.id)}>
-                                <i class="bi bi-trash3"></i>
-                            </button>
-                        </div>
-                    </div>
-                ))
-            ) : (
-                <div className="no-cart">
-                    <p>Tu carrito está vacío</p>
-                </div>
-            )}
+                    )
+            }
         </div>
     );
 };
